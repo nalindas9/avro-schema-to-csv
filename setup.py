0d91cdde98144cfd02119312b701ebfc5e87c22c
Nalin Das <nalindas9@gmail.com>
2022-04-03 23:59:04 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='avro-schema-to-json',
+    version='0.1.0',
+    description='Convert Avro schema to JSON',
+    author='Nalin Das'
+)
